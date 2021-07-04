c3cab30d9fcde1aa6a14f8b2cb2cf24272694cbe
aymeric dominique <aymeric.dominique@gmail.com>
2021-07-04 12:01:15 +0200

    fix double connection

--- src/commands/commands.module.ts
+++ src/commands/commands.module.ts
@@ -1,17 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppGateway } from 'src/app.gateway';
 import { PastriesModule } from 'src/pastries/pastries.module';
+import { SharedModule } from 'src/shared/shared.module';
 import { CommandsController } from './commands.controller';
 import { CommandsService } from './commands.service';
 import { Command, CommandSchema } from './schemas/command.schema';
 
 @Module({
   imports: [
+    SharedModule,
     PastriesModule,
     MongooseModule.forFeature([{ name: Command.name, schema: CommandSchema }]),
   ],
   controllers: [CommandsController],
-  providers: [CommandsService, AppGateway],
+  providers: [CommandsService],
 })
 export class CommandsModule {}
--- src/pastries/pastries.module.ts
+++ src/pastries/pastries.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppGateway } from 'src/app.gateway';
+import { SharedModule } from 'src/shared/shared.module';
 import { PastriesController } from './pastries.controller';
 import { PastriesService } from './pastries.service';
 import { Pastry, PastrySchema } from './schemas/pastry.schema';
 
 @Module({
   imports: [
+    SharedModule,
     MongooseModule.forFeature([{ name: Pastry.name, schema: PastrySchema }]),
   ],
   controllers: [PastriesController],
-  providers: [PastriesService, AppGateway],
+  providers: [PastriesService],
   exports: [PastriesService],
 })
 export class PastriesModule {}
