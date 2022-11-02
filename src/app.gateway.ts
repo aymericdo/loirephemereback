d5a07687d793e486cd20f2504a2961e27e681f01
aymeric dominique <aymeric.dominique@gmail.com>
2022-11-02 22:58:34 +0100

    try

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -13,7 +13,7 @@ import WebSocket = require('ws');
 import { CommandDocument } from './commands/schemas/command.schema';
 import webpush = require('web-push');
 
-@WebSocketGateway(3002)
+@WebSocketGateway()
 export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
